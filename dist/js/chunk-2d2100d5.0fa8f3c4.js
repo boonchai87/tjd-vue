9156cc15df63d315e1f1185861d7b216a63c2236
Boonchai Wedwanitsatheiin <neng87@gmail.com>
2021-12-10 19:58:40 +0700

    add dist folder

---
+++ dist/js/chunk-2d2100d5.0fa8f3c4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2100d5"],{b5f2:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"container"},[n._v(" Test Axios ")])},s=[],r={name:"home",components:{},data:function(){return{username:"neng",number:5,arrayType:["33","ddd"]}},methods:{}},o=r,u=t("2877"),c=Object(u["a"])(o,a,s,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d2100d5.0fa8f3c4.js.map
